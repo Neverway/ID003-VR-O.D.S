aee05643c785928ea1c49d02378aa05b95ad935f
Lizband <trash.block2@gmail.com>
2020-10-25 16:21:35 -0400

    Added VR IK ladders, Cleaned up main scenes assets

---
+++ Assets/ClimbInteract.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ClimbInteract : XRBaseInteractable
+{
+    protected override void OnSelectEnter(XRBaseInteractor interactor)
+    {
+        base.OnSelectEnter(interactor);
+        if (interactor is XRDirectInteractor)
+        {
+            IKClimb.climbingHand = interactor.GetComponent<XRController>();
+        }
+    }
+
+    protected override void OnSelectExit(XRBaseInteractor interactor)
+    {
+        base.OnSelectExit(interactor);
+        if (interactor is XRDirectInteractor)
+        {
+            if (IKClimb.climbingHand && IKClimb.climbingHand.name == interactor.name)
+            {
+                IKClimb.climbingHand = null;
+            }
+        }
+    }
+}
