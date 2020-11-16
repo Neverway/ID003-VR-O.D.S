3e00123fec916184a44e2817d26838462c983d3f
Memphis P <bixthegamedev@gmail.com>
2020-11-16 01:27:15 -0500

    Added Toggle Triggers for UI Screens

---
+++ Assets/4.Scripts/ScreenToggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenToggle : MonoBehaviour
+{
+    public GameObject PlayerHead;
+    public GameObject ScreenUI;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            ScreenUI.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            ScreenUI.SetActive(false);
+        }
+    }
+}
