716275e816caffe0d0a32b7a644377cb29efd31a
Lizband <Trash.block2@gmail.com>
2020-11-17 02:13:22 -0500

    Fixed crowbar collision, Fixed object layers, Added fighter engine task

---
+++ Assets/4.Scripts/InteractableBlowTorch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBlowTorch : MonoBehaviour
+{
+    public GameObject flame;
+    public GameObject flameTrigger;
+    public bool blowTorchOn=false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (blowTorchOn)
+        {
+            flame.SetActive(true);
+            flameTrigger.SetActive(true);
+        }
+
+        if (!blowTorchOn)
+        {
+            flame.SetActive(false);
+            flameTrigger.SetActive(false);
+        }
+    }
+
+    public void ToggleTorch()
+    {
+        if (blowTorchOn)
+        {
+            blowTorchOn = false;
+        }
+        else if (!blowTorchOn)
+        {
+            blowTorchOn = true;
+        }
+    }
+}
---
+++ Assets/TaskFighterEngine.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskFighterEngine : MonoBehaviour
+{
+    public bool taskActive;
+    public bool hasStatusIndicator;
+    public GameObject pilotLight;
+    public GameObject statusIndicator;
+    public Material indicatorBad;
+    public Material indicatorGood;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Flame"))
+        {
+            DeactivateTask();
+        }
+    }
+
+
+    public void ActivateTask()
+    {
+        taskActive = true;
+        pilotLight.SetActive(false);
+        if (hasStatusIndicator)
+        {
+            statusIndicator.GetComponent<MeshRenderer>().material = indicatorBad;
+        }
+    }
+
+
+    public void DeactivateTask()
+    {
+        taskActive = false;
+        pilotLight.SetActive(true);
+        if (hasStatusIndicator)
+        {
+            statusIndicator.GetComponent<MeshRenderer>().material = indicatorGood;
+        }
+    }
+}
