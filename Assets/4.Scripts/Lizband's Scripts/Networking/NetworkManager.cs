b103d709ed396ac15e6c850868ddaf513e1aa41b
Lizband <Trash.block2@gmail.com>
2020-11-22 18:23:36 -0500

    Added multiplayer networking

---
+++ Assets/4.Scripts/Lizband's Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public byte serverMaxPlayers = 4;
+    public bool serverIsVisible = true;
+    public bool serverIsOpen = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("[ID003] : Trying to connect to server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("[ID003] : Connection successful!");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = serverMaxPlayers;
+        roomOptions.IsVisible = serverIsVisible;
+        roomOptions.IsOpen = serverIsOpen;
+
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("[ID003] : Joined room!");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        Debug.Log("[ID003] : A new player has connected!");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
