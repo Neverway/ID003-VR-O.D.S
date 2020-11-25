63877b03f9dd0977c2375166e16b193163db7c31
Lizband <Trash.block2@gmail.com>
2020-11-25 02:43:55 -0500

    Updated title level

---
+++ Assets/4.Scripts/Lizband's Scripts/Button Like/UITrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UITrigger : MonoBehaviour
+{
+    public UnityEvent onUITriggerEnter;
+    public UnityEvent onUITriggerExit;
+
+    private void OnTriggerEnter()
+    {
+        onUITriggerEnter.Invoke();
+    }
+
+
+	private void OnTriggerExit()
+	{
+        onUITriggerExit.Invoke();
+	}
+}
---
+++ Assets/4.Scripts/Lizband's Scripts/TitleFighterSpin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleFighterSpin : MonoBehaviour
+{
+    public float stepRateX = 1;
+    public float stepRateY = 1;
+    public float stepRateZ = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion localRotation = Quaternion.Euler(transform.rotation.x - stepRateX, 0f, 0f);
+        transform.rotation = transform.rotation * localRotation;
+    }
+}
