c3bcf6476835b2fd73d47fc634e4966715a47cfa
Memphis <bixthegamedev@gmail.com>
2021-03-18 05:40:20 -0400

    Added task system
    System for creating tasks / monitoring them

---
+++ Assets/4.Scripts/Bix's Better Scripts/Tasks/Base Task System/TaskControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskControl : MonoBehaviour
+{
+    public string TaskName;
+    public bool Completed;
+    void Start()
+    {
+        name = TaskName;
+    }
+}
---
+++ Assets/4.Scripts/Bix's Better Scripts/Tasks/Base Task System/TaskManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TaskManager : MonoBehaviour
+{
+    public GameObject[] Tasks;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
